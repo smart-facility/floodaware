212947613581256ed293561776beddb4f744d081
natohutch <toadestra@gmail.com>
2021-06-03 14:10:22 +1000

    fixed error when clearing potential old database

--- pg_init.sh
+++ pg_init.sh
@@ -1,5 +1,5 @@
+docker exec -d floodaware_db_1 bash -c "psql -U postgres -c 'DROP DATABASE floodaware'"
 docker exec -ti floodaware_db_1 bash -c "\
-psql -U postgres -c 'DROP DATABASE floodaware' &&\
 psql -U postgres -c 'CREATE DATABASE floodaware' &&\
 pg_restore -U postgres -d floodaware -cC floodawaredb.sql &&\
 exit
